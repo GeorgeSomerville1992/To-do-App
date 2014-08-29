a7b59f9b08932f0e6987584ad04fdf2cb7aeca5d
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-08-29 12:59:56 +0100

    promise install

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,47 @@
 /** @jsx React.DOM */
 
 React.renderComponent(
-  <h1>Hello, poo!</h1>,
+  <h1>Hello, world first render!</h1>,
   document.getElementById('example')
+);
+
+
+
+var newtodoList = React.createClass({
+  render: function() {
+    return (
+      <div className="todoList">
+        Hello, world! I am a todoList.
+      </div>
+    );
+  }
+});
+
+var NewToDoForm = React.createClass({
+  render: function() {
+    return (
+      <div className="todoForm">
+        Hello, world! I am a NewToDoForm
+      </div>
+    );
+  }
+});
+
+// these goes at the bottom as js needs to render stuff first (not put a initalize function???) 
+var todo = React.createClass({
+  render: function() {
+    return (
+      <div className="TodoBox">
+        <h1>Todos</h1>
+        <newtodoList/> // these are variables!!!
+        <NewToDoForm  /> // so we can put classnames as actualt tags? this is weirld. 
+
+      </div>
+    );
+  }
+})
+
+React.renderComponent(
+  todo(null),
+  document.getElementById('content')
 );
