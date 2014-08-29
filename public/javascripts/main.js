ab09e4ffe66fb007d38c2b53f15fef413f491f82
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-08-29 15:17:20 +0100

    added and shown basic data request from hash

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,14 +4,25 @@ React.renderComponent(
   <h1>Hello, world first render!</h1>,
   document.getElementById('example')
 );
-
+var data = [
+  {description: "cleanup", text: "This is one tjbjkbodo "},
+  {description: "get a life", text: "This is *anhbhbvother* todo"}
+];
 
 
 var newtodoList = React.createClass({
   render: function() {
+    var todoNodes = this.props.data.map(function (todo) {
+      return (
+        <Todo author={todo.description}>
+          {todo.text}
+        </Todo>
+      );
+    });
+    // taking all of the abocve from the data and shoving ti in below
     return (
       <div className="todoList">
-        Hello, world! I am a todoList.
+        {todoNodes}
       </div>
     );
   }
@@ -26,6 +37,24 @@ var NewToDoForm = React.createClass({
     );
   }
 });
+var converter = new Showdown.converter();
+var Todo = React.createClass({
+  render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
+    return (
+      <div className="todo">
+        <h2 className="tododescription">
+          {this.props.description}
+        </h2>
+        
+        <h3 className="tododate">
+          {this.props.dueby}
+        </h3>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}}/>// to end the dic???
+      </div> // react does not like this at alll
+    );
+  }
+});
 
 // these goes at the bottom as js needs to render stuff first (not put a initalize function???) 
 var todo = React.createClass({
@@ -33,7 +62,7 @@ var todo = React.createClass({
     return (
       <div className="TodoBox">
         <h1>Todos</h1>
-        <newtodoList/> // these are variables!!!
+        <newtodoList data={this.props.data}/> // these are variables!!!
         <NewToDoForm  /> // so we can put classnames as actualt tags? this is weirld. 
 
       </div>
@@ -42,6 +71,6 @@ var todo = React.createClass({
 })
 
 React.renderComponent(
-  todo(null),
+  <todo data={data} />,
   document.getElementById('content')
 );
